8494341bd86d466241a7e9d7c9b31f57fe802702
Madelin Stueck <made2130@gmail.com>
2026-03-23 19:43:46 -0400

    Update 03_multiqc.sh

--- scripts/03_multiqc.sh
+++ scripts/03_multiqc.sh
@@ -3,9 +3,12 @@
 
 set -euo pipefail
 
+#set paths
 FASTQC=$SCRATCH/fastqc_results
 OUT=$SCRATCH/multiqc_report
 
+#generate output directory 
 mkdir -p "$OUT"
 
+#run multiqc on collective fastqc results
 multiqc "$FASTQC" -o "$OUT"
