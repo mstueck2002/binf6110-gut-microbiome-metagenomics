acb81b611e3142d94192bf11d140ad445c4bbae0
Madelin Stueck <made2130@gmail.com>
2026-03-23 19:42:14 -0400

    Update 01_download_sra.sh

--- scripts/01_download_sra.sh
+++ scripts/01_download_sra.sh
@@ -5,9 +5,12 @@
 #SBATCH --time=8:00:00
 #SBATCH --output=sra_download.out
 
+#load necessary modules
 module load sra-toolkit
 
+#prefetch SRR IDs
 xargs -a srr_ids.txt -n 1 -P 4 prefetch
 
+#find associated SRA and retrieve paired fastq files
 find . \( -name "*.sra" -o -name "*.sralite" \) -print0 | \
 xargs -0 -n 1 -P 2 fasterq-dump --split-files --threads 8
