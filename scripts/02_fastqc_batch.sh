d6e89e2c56667b08fc13c80403e237e406ac933a
Madelin Stueck <made2130@gmail.com>
2026-03-23 19:43:05 -0400

    Update 02_fastqc_batch.sh

--- scripts/02_fastqc_batch.sh
+++ scripts/02_fastqc_batch.sh
@@ -6,13 +6,18 @@
 #SBATCH --array=1-147
 #SBATCH --output=fastqc_%A_%a.out
 
+#load necessary modules
 module load fastqc
 
+#set paths
 FASTQ_DIR=$SCRATCH/fastq_files
 OUT_DIR=$SCRATCH/fastqc_results
 
+#generate output directory
 mkdir -p $OUT_DIR
 
+#specify files in array
 FILE=$(ls $FASTQ_DIR/*.fastq | sed -n "${SLURM_ARRAY_TASK_ID}p")
 
+#run fastqc using parallel array
 fastqc -o $OUT_DIR $FILE
